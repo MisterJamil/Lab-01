ec442254a346ecd31c83d06f49e701405f33c3a6
Jamil <mr.jamilsaliba@gmail.com>
2016-09-01 14:14:49 -0700

    Initial Commit
    Completed Lab-01, simple print statement, my first programming project
    in C++

---
+++ Lab 01/Lab_01.cpp
@@ -0,0 +1,33 @@
+// Lab 01.cpp : Defines the entry point for the console application.
+// 
+/* Author: Jamil Saliba
+Date: 8-25-16
+Description: Lab 01 Tree
+*/
+
+#include "header.h"
+
+
+int main()
+{
+	printf("        *\n");
+	printf("       * *\n");
+	printf("      *   *\n");
+	printf("     *     *\n");
+	printf("    * *   * *\n");
+	printf("     *     *\n");
+	printf("    *       *\n");
+	printf("   * *     * *\n");
+	printf("    *       *\n");
+	printf("   *         *\n");
+	printf("  *           *\n");
+	printf(" ******  *******\n");
+	printf("      *  *\n");
+	printf("      *  *\n");
+	printf("      ****\n");
+	printf("\n\n\n\n");
+
+
+
+	return 0;//Exit program
+}
---
+++ Lab 01/Lab_01.h
@@ -0,0 +1,9 @@
+// Lab 01.cpp : Defines the entry point for the console application.
+// 
+/* Author: Jamil Saliba
+Date: 8-25-16
+Description: Lab 01 Tree
+*/
+#pragma once
+#include <stdio.h>
+#include <tchar.h>
